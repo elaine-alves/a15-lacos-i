849007add6f0cc0c6cdc4b59daa79416eef86979
unknown <elainecristinedeoliveiraalves@gmail.com>
2022-08-15 22:33:07 -0300

    Entrega do exercício para praticar A015-Laços I

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let coxinha = prompt ("Quer comer mais coxinha?= \n S: Sim \n N: Não")
+let conta = coxinha + 2.50
+while (coxinha!==0){
+conta =  conta + coxinha
+coxinha = prompt ("Quer comer mais coxinha? \n S: Sim \n N: Não")
+}
+console.log(conta);
+//NÃO CONSEGUI :'(
---
+++ exercicio2/script.js
@@ -0,0 +1,5 @@
+
+const numero = +prompt("Digite um número que lhe mostro a tabuada:")
+for(let i=0; i<=10;i++){
+console.log(numero,"x",i,"=",numero*i)
+}
